f7987c19bd443f2c6214ed5119705e71fc9ce1bd
Dharmendra <mahatodharmendra726@gmail.com>
2024-09-27 16:48:59 +0530

    TMDB API Integration

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,14 +3,15 @@ import Header from "./Header";
 import { checkValidData } from "../utils/validate";
 import {createUserWithEmailAndPassword,signInWithEmailAndPassword, updateProfile} from "firebase/auth";
 import {auth} from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
+
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { USER_AVATAR } from "../utils/constants";
 
 const Login = () => {
    const [isSignInForm, setIsSignInForm] = useState(true);
    const [errorMessage,setErrorMessage] = useState(null);
-   const navigate = useNavigate();
+  
    const dispatch = useDispatch();
    
    const name = useRef(null);
@@ -37,7 +38,7 @@ const Login = () => {
            .then((userCredential) => {
             const user = userCredential.user;
             updateProfile(user, {
-               displayName: name.current.value, photoURL: "https://avatars.githubusercontent.com/u/118893589?v=4",
+               displayName: name.current.value, photoURL:USER_AVATAR,
              }).then(() => {
                // Profile updated!
                // ...
@@ -50,8 +51,7 @@ const Login = () => {
                   photoURL:photoURL,
                })
             );
-               
-               navigate("/browse");
+             
              }).catch((error) => {
                // An error occurred
                // ...
@@ -71,8 +71,8 @@ const Login = () => {
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
-    console.log(user);
-    navigate("/browse");
+   // console.log(user);
+    
   })
   .catch((error) => {
     const errorCode = error.code;
@@ -101,6 +101,7 @@ const Login = () => {
           <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
           {!isSignInForm && (
           <input 
+          ref ={name}
           type="text" 
           placeholder="Full Name"
           className="p-4 my-4 w-full bg-gray-700"
---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+
+export const USER_AVATAR = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7Kx-vplluOaYeII5PbHMg86NQRRs3AEMrj1i2-J_UFvOOrC-q0tgKDqVSlg&s"
+
+export const API_OPTIONS = {
+    method: "GET",
+    headers: {
+        accept: "application/json",
+        Authorization:
+         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjNmNzgyODA4NTFjNjI3NmZiY2JkYjg0ODM1ZTJjYiIsIm5iZiI6MTcyNTc3NTIzMC40MDkwMDQsInN1YiI6IjY2ZGQzMTcxMGJjZWEwOTNhNzc4Nzc1NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-5bxw2NxVPavZ1Fb-RX0Kyoyhhu3c19Ipr0W1eIuHr4"
+    }, 
+};
